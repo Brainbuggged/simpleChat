9c93b4251bc21e906009182172d5654724c574ef
BrainyB <brainbuggged@gmail.com>
2018-04-16 23:20:13 +0300

    Static fields were made none-static and empty spaces were removed

--- MyClient/MyClient/Form1.cs
+++ MyClient/MyClient/Form1.cs
@@ -13,91 +13,52 @@ namespace MyClient
 {
     public partial class Form1 : Form
     {
+        static string Host = Dns.GetHostName();
+        string LocalAddress = Dns.GetHostEntry(Host).AddressList[0].ToString();
+        Socket tcpSocket = new Socket(AddressFamily.InterNetwork,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
         public Form1()
         {
             InitializeComponent();
         }
 
-        static IPAddress mcastAddress;
-        static int mcastPort;
-        
-        
-    
-        XDocument doc;
-
-          static string Host = Dns.GetHostName();
-   static   string IP = Dns.GetHostByName(Host).AddressList[0].ToString();
-
-
-   Socket tcpSocket = new Socket(AddressFamily.InterNetwork,
-                                   SocketType.Stream,
-                                   ProtocolType.Tcp);
-
         public void GetServerAddress()
         {
             byte[] bytes = new byte[500];
-            mcastAddress = IPAddress.Parse("224.12.12.12");
-            mcastPort = 4000;
-
-
+             IPAddress mcastAddress = IPAddress.Parse("224.12.12.12");
+             int  mcastPort = 4000;
             Socket mcastSocket = new Socket(AddressFamily.InterNetwork,
                                         SocketType.Dgram,
                                         ProtocolType.Udp);
-
             EndPoint serverEndPoint = new IPEndPoint(mcastAddress, mcastPort);
 
             IPEndPoint multicastEndPoint = new IPEndPoint(mcastAddress, mcastPort);
            
-             MulticastOption mcastOption = new MulticastOption(mcastAddress, IPAddress.Parse(IP));
+             MulticastOption mcastOption = new MulticastOption(mcastAddress, IPAddress.Parse(LocalAddress));
 
             mcastSocket.SetSocketOption(SocketOptionLevel.IP,
                                         SocketOptionName.AddMembership,
                                         mcastOption);
-            
-            mcastSocket.SendTo(Encoding.UTF8.GetBytes(IP + ":" + mcastPort), multicastEndPoint);
-
-          
-           
-
+            mcastSocket.SendTo(Encoding.UTF8.GetBytes(LocalAddress + ":" + mcastPort), multicastEndPoint);
             mcastSocket.ReceiveFrom(bytes, ref serverEndPoint);
-
-
             Thread.Sleep(500);
             tcpSocket.Connect(serverEndPoint);
             MessageBox.Show(serverEndPoint.ToString());
-        
-
-         
-            
-        
         }
-
-
-
 
          void SendMessage(string message)
         {
-           
-          
-               
-
-                doc = new XDocument(
+            var sendMessageDocument = new XDocument(
                   new XElement("msg",
                   new XElement("data", new XAttribute("key", "Type"), new XAttribute("value", "Send")),
                   new XElement("data", new XAttribute("key", "Text"), new XAttribute("value", message))));
 
-
-
-              
-                
-              tcpSocket.Send(Encoding.UTF8.GetBytes(doc.ToString()));
-              
-            
-         
-
-          
+              tcpSocket.Send(Encoding.UTF8.GetBytes(sendMessageDocument.ToString()));
          }
+
         delegate void Receive(string text);
+
         public void DoSmth(string text)
         {
             richTextBox1.AppendText("\n"+text);
@@ -111,25 +72,13 @@ namespace MyClient
             bytes = bytes.Take(tcpSocket.Receive(bytes)).ToArray();
             string message = Encoding.UTF8.GetString(bytes);
             return message;
-           
-      
-
         }
 
-
-
-        private  void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
-           
-   
-           
             GetServerAddress();
             timer1.Start();
-          
-
-
-    }
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -140,7 +89,6 @@ namespace MyClient
         {
 
         }
-
 
         private void timer1_Tick(object sender, EventArgs e)
         {
